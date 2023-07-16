90f6cb6ca78dba47940b58c535e1cffdaeaba193
GDharmik9 <GDg11104@gmail.com>
2023-07-16 13:01:16 +0530

    fix(ui): for github page, added basename

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,6 +1,6 @@
 import DashboardTable from 'common/components/molecules/DashboardTable'
 import SignIn from 'pages/SignIn'
-import { Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import React from 'react'
 import ForgetPassword from 'pages/ForgetPassword'
 import ResetPassword from 'pages/ResetPassword'
@@ -9,13 +9,15 @@ import Register from 'pages/Register'
 const App = () => {
   return (
     <div className="App">
-      <Routes>
-        <Route path="/" element={<Register />}></Route>
-        <Route path="/login" element={<SignIn />}></Route>
-        <Route path="/dashboard" element={<DashboardTable />}></Route>
-        <Route path="/forget-password" element={<ForgetPassword />}></Route>
-        <Route path="/reset-password" element={<ResetPassword />}></Route>
-      </Routes>
+      <BrowserRouter basename="/interview-project">
+        <Routes>
+          <Route path="/" element={<Register />}></Route>
+          <Route path="/login" element={<SignIn />}></Route>
+          <Route path="/dashboard" element={<DashboardTable />}></Route>
+          <Route path="/forget-password" element={<ForgetPassword />}></Route>
+          <Route path="/reset-password" element={<ResetPassword />}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   )
 }
